5bdc6b8b3489bc92737e399628c248e7877b7e53
goto <gogo8bit@yahoo.co.jp>
2023-03-17 16:26:26 +0900

    画像データ、画像パス取得を追加

--- wiskey_scraping.py
+++ wiskey_scraping.py
@@ -1,4 +1,6 @@
 import requests
+import uuid
+import os
 from bs4 import BeautifulSoup
 
 try:
@@ -16,30 +18,28 @@ try:
     # csvファイルを開く
     with open('whiskey.csv', mode='w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['画像URL', '商品名', 'メーカー名', '商品種類', 'アルコール度数'])
+        writer.writerow(['画像パス', '商品名称', 'メーカー名', '商品種類', 'アルコール度数'])
 
         index = 1
         # 商品情報を含むすべての要素を取得する
         items = soup.select('tr[class="tr-border"]')
         for item in items:
-            # # 商品画像を取得して保存する
-            # img_url = item.find('img')['src']
-            # img_response = requests.get(img_url)
-            # with open('images/' + item.find('p', {'class': 'p-item__ttl'}).text.strip() + '.jpg', mode='wb') as img_file:
-            #     img_file.write(img_response.content)
-            # img_path = 'images/' + item.find('p', {'class': 'p-item__ttl'}).text.strip() + '.jpg'
-
             # 商品情報を取得する(tr-borderが3つで1セットであるため)
             if index % 3 == 1:
                 maker = item.find('span').getText().strip()
             elif index % 3 == 2:
+                img_url = item.find('a', {'class': 'withIcnLimited'}).find('img')['src']
+                img_response = requests.get(img_url)
+                with open(str('./images/')+str(uuid.uuid4())+str('.jpeg'),'wb') as file:
+                        file.write(img_response.content)
+                file_path = os.path.abspath(file.name)
                 name = item.select('img')[0].get('alt')
                 kind = item.find('span', {'class': 'sortBox'}).find('a').getText().strip()
                 degreeHTML = item.find('td', {'class': 'end'}).find('span', {'class': 'typeClk'})
                 degree = degreeHTML.getText().strip() if degreeHTML is not None else ''
 
                 # 取得したデータをcsvファイルに書き込む
-                writer.writerow([name, maker, kind, degree])
+                writer.writerow([file_path,name, maker, kind, degree])
 
             index+=1
 
